2df4e0145518c3469fb4ac3d5074c014602f8dad
Brian Ingles <github@emeraldwalk.com>
2015-04-25 16:11:31 -0400

    Iinitial commit - Moving Perl Language Service into its own repository.

---
+++ Emeraldwalk.PerlLanguageService._Tests/Perl/Parsers/TokenMatcherTester.cs
@@ -0,0 +1,29 @@
+﻿using Emeraldwalk.PerlLanguage.Perl.Parsing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emeraldwalk.PerlLanguageService._Tests
+{
+    [TestClass]
+    public class TokenMatcherTester
+    {
+        [TestMethod]
+        public void MatchesCurlyBraces()
+        {
+            string text = 
+@"helloworld() {
+    if(true) {
+    }
+}";
+
+            TokenMatcher matcher = new TokenMatcher("{", "}");
+            matcher.Parse(text);
+
+            Assert.AreEqual(2, matcher.TokenPairs.Count);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Emeraldwalk.PerlLanguageService
-Perl language service Visual Studio extension.
+# Emeraldwalk - Language Services README
+===========
+## Perl Language Service
+
+Emeraldwalk.LanguageServices is a **Perl language service** extension for Visual Studio.
+
+### Implemented Features
+* Perl syntax highlighting
+* Brace matching
+* Outlining
